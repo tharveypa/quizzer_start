d422f45331eb73c70dd7bf6f9f3f0d7e5b6ed5ae
Joyboy97 <winnerstar29@gmail.com>
2022-10-27 13:50:20 -0400

    username fix

--- src/quizzer/QuizList.tsx
+++ src/quizzer/QuizList.tsx
@@ -53,7 +53,6 @@ export const QuizList = ({
                             resetView={resetQuizView}
                         ></QuizView>
                     );
-                    //suss
                 }
             })}
         </div>
