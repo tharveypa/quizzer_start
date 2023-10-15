1db3b0b10e492cb1ac5547217761fce969e86de3
Sam Ferguson <srfergsn@gmail.com>
2023-10-15 18:00:06 -0400

    New commits

--- src/quizzer/QuizList.tsx
+++ src/quizzer/QuizList.tsx
@@ -6,52 +6,55 @@ import "./QuizList.css";
 import { QuizView } from "./QuizView";
 
 export const QuizList = ({
-    quizzes,
-    editQuiz,
-    deleteQuiz,
-    showModal
-}: {quizzes: Quiz[], editQuiz: (index: number, quiz: Quiz) => void, 
-    deleteQuiz: (index: number) => void, showModal: () => void, 
+  quizzes,
+  editQuiz,
+  deleteQuiz,
+  showModal,
+}: {
+  quizzes: Quiz[];
+  editQuiz: (index: number, quiz: Quiz) => void;
+  deleteQuiz: (index: number) => void;
+  showModal: () => void;
 }) => {
-    const [displayId, setDisplayId] = useState<null | number>(null);
+  const [displayId, setDisplayId] = useState<null | number>(null);
 
-    const handleQuizView = (id: number) => {
-        setDisplayId(id);
-    };
+  const handleQuizView = (id: number) => {
+    setDisplayId(id);
+  };
 
-    const resetQuizView = () => {
-        setDisplayId(null);
-    };
+  const resetQuizView = () => {
+    setDisplayId(null);
+  };
 
-    return (
-        <div className="quiz_list">
-            {!displayId && (
-                <>
-                    {quizzes.map((quiz: Quiz) => (
-                        <QuizCard
-                            key={quiz.id}
-                            quiz={quiz}
-                            handleClick={handleQuizView}
-                        ></QuizCard>
-                    ))}
-                    <Button className="add_btn" onClick={showModal}>
-                        Add New Quiz
-                    </Button>
-                </>
-            )}
-            {quizzes.map((quiz: Quiz) => {
-                if (displayId === quiz.id) {
-                    return (
-                        <QuizView
-                            key={quiz.id}
-                            quiz={quiz}
-                            editQuiz={editQuiz}
-                            deleteQuiz={deleteQuiz}
-                            resetView={resetQuizView}
-                        ></QuizView>
-                    );
-                }
-            })}
-        </div>
-    );
+  return (
+    <div className="quiz_list">
+      {!displayId && (
+        <>
+          {quizzes.map((quiz: Quiz) => (
+            <QuizCard
+              key={quiz.id}
+              quiz={quiz}
+              handleClick={handleQuizView}
+            ></QuizCard>
+          ))}
+          <Button className="add_btn" onClick={showModal}>
+            Add New Quiz
+          </Button>
+        </>
+      )}
+      {quizzes.map((quiz: Quiz) => {
+        //if (displayId === quiz.id) {
+        return (
+          <QuizView
+            key={quiz.id}
+            quiz={quiz}
+            editQuiz={editQuiz}
+            deleteQuiz={deleteQuiz}
+            resetView={resetQuizView}
+          ></QuizView>
+        );
+        //}
+      })}
+    </div>
+  );
 };
